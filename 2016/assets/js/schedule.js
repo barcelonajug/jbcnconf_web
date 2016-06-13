e71d802fd834c75f406f6df56317f96c8059e8df
Jordi Pons <jordi.pons@netcentric.biz>
2016-06-13 12:30:21 +0200

    Changed room location in the schedule

--- 2016/assets/js/schedule.js
+++ 2016/assets/js/schedule.js
@@ -13,8 +13,6 @@ $(document).ready(function () {
 						}
 					}
 				}
-				divspeaker=divspeaker+"</span><span class='location'>(Room: "+getRoom(val.scheduleId);
-				divspeaker=divspeaker+")</span></div></a>";
 				if(val.scheduleId=='#Fri-Keynote' || val.scheduleId=='#Sat-Keynote'){
 					divspeaker="<span class='keynote'>Keynote [Auditorium]</span>"+divspeaker;
 				}
@@ -30,32 +28,6 @@ $(document).ready(function () {
 	  });
 	});
 });
-
-function contains(scheduleId, day) {
-	return scheduleId.indexOf(day) > -1;
-}
-
-function getRoom(scheduleId) {
-	if (contains(scheduleId, "THU-TC1") || contains(scheduleId, "SAT-TC1")) {
-		return "20.019";
-	} else if (contains(scheduleId, "THU-TC2") || contains(scheduleId, "SAT-TC2")) {
-		return "20.021";
-	} else if (contains(scheduleId, "THU-TC3") || contains(scheduleId, "SAT-TC3")) {
-		return "20.023";
-	} else if (contains(scheduleId, "THU-TC4")) {
-		return "20.027";
-	} else if (contains(scheduleId, "SAT-TC4")) {
-		return "20.025";
-	} else if (contains(scheduleId, "FRI-TC1")) {
-		return "40.002";
-	} else if (contains(scheduleId, "FRI-TC2")) {
-		return "40.004";
-	} else if (contains(scheduleId, "FRI-TC3")) {
-		return "40.006";
-	} else if (contains(scheduleId, "FRI-TC4")) {
-		return "40.008";
-	}
-}
 
 $('#showThursday').click(function(){
  $('#saturday').hide();
