0676e3bc10f16a520b73ad0b8262e2615eac231f
Jordi Pons <jordi.pons@netcentric.biz>
2016-06-13 12:03:45 +0200

    Added Room numbers into the schedule

--- 2016/assets/js/schedule.js
+++ 2016/assets/js/schedule.js
@@ -5,15 +5,16 @@ $(document).ready(function () {
 			if($(val.scheduleId).children('span').hasClass('pending')){
 				divspeaker="<a href='"+val.url+"'><div><span class='title'>"+val.talk.title+"</span><span class='speaker'>"+val.name;
 				if(val.talk.cospeakerref!=null){
-						for(var i=0;i<data.speakers.length;i++){
-							valspeaker2 = data.speakers[i];
-							if(valspeaker2.ref == val.talk.cospeakerref){
-								divspeaker=divspeaker+ " & " +valspeaker2.name ;			
-								break;
-							}
+					for(var i=0;i<data.speakers.length;i++){
+						valspeaker2 = data.speakers[i];
+						if(valspeaker2.ref == val.talk.cospeakerref){
+							divspeaker=divspeaker+ " & " +valspeaker2.name ;			
+							break;
 						}
 					}
-				divspeaker=divspeaker+"</span></div></a>";
+				}
+				divspeaker=divspeaker+"</span><span class='location'>(Room: "+getRoom(val.scheduleId);
+				divspeaker=divspeaker+")</span></div></a>";
 				if(val.scheduleId=='#Fri-Keynote' || val.scheduleId=='#Sat-Keynote'){
 					divspeaker="<span class='keynote'>Keynote [Auditorium]</span>"+divspeaker;
 				}
@@ -29,6 +30,32 @@ $(document).ready(function () {
 	  });
 	});
 });
+
+function contains(scheduleId, day) {
+	return scheduleId.indexOf(day) > -1;
+}
+
+function getRoom(scheduleId) {
+	if (contains(scheduleId, "THU-TC1") || contains(scheduleId, "SAT-TC1")) {
+		return "20.019";
+	} else if (contains(scheduleId, "THU-TC2") || contains(scheduleId, "SAT-TC2")) {
+		return "20.021";
+	} else if (contains(scheduleId, "THU-TC3") || contains(scheduleId, "SAT-TC3")) {
+		return "20.023";
+	} else if (contains(scheduleId, "THU-TC4")) {
+		return "20.027";
+	} else if (contains(scheduleId, "SAT-TC4")) {
+		return "20.025";
+	} else if (contains(scheduleId, "FRI-TC1")) {
+		return "40.002";
+	} else if (contains(scheduleId, "FRI-TC2")) {
+		return "40.004";
+	} else if (contains(scheduleId, "FRI-TC3")) {
+		return "40.006";
+	} else if (contains(scheduleId, "FRI-TC4")) {
+		return "40.008";
+	}
+}
 
 $('#showThursday').click(function(){
  $('#saturday').hide();
