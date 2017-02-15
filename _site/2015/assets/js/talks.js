5d5878ad3863cd1bd2f4c80625d3c961f146f848
Miklos Csere <miklos.csere@netcentric.biz>
2017-02-15 13:42:12 +0100

    Speaker page and ruby plugin

---
+++ _site/2015/assets/js/talks.js
@@ -0,0 +1,122 @@
+$(document).ready(function () {
+	$.getJSON( "assets/json/speakers.json", function( data ) {
+	  divtalk = "<ul style='list-style-type:none'>";
+	  data.speakers.sort(function(a, b){
+			if(a.talk.title < b.talk.title) return -1;
+			if(a.talk.title > b.talk.title) return 1;
+			return 0;
+	  })
+	  $.each( data.speakers, function( key, val ) {
+	  	if (val.enabled == 1 && val.talk != "" && divtalk.indexOf(val.talk.title) < 0) {
+			if(val.url!=""){
+				divtalk += "<li><a href='"+val.url+"'><span class='title'>"+val.talk.title +"</span> by <span class='speaker'>"+ val.name ;
+				if(val.cospeakerref!=null){
+					for(var i=0;i<data.speakers.length;i++){
+						valspeaker2 = data.speakers[i];
+						if(valspeaker2.ref == val.cospeakerref){
+							divtalk += " & " +valspeaker2.name ;			
+						}
+					}
+				}
+				divtalk += "</span></a>";
+			}
+			if (val.talk.tags) {
+			  $.each(val.talk.tags, function (index,value) {
+			  	divtalk += "<span class='label tag "+getFamilyForTag(value)+"'>" + value + "</span>";
+			  });
+			}
+			if(val.talk.slides){
+				divtalk += "<a href='"+val.talk.slides+"'><span class='slides' title='Slides'><i class='fa fa-picture-o fa-1x'></i></span></a>";
+			}
+			if(val.talk.video){
+				divtalk += "<a href='"+val.talk.video+"'><span class='video' title='Video'><i class='fa fa-film fa-1x'></i></span></a>";
+			}
+			divtalk += "</li>";
+
+			
+		}
+	  });
+	divtalk += "</ul>"; 
+	$(divtalk).appendTo( "#grid-talks" );
+	});
+});
+
+function getFamilyForTag(tag){
+	family = "unknown";
+	switch (tag) {
+		case "Messaging" : 
+		case "Scalability" :
+		case "Cloud":
+		case "Integration":
+		case "Design":
+		case "Architecture":
+			family = "Architecture"; break;
+			
+		case "Testing" : 
+		case "Devops" :
+		case "ContinuousDelivery":
+		case "ContinuousIntegration":
+			family = "Metodology"; break;
+
+		case "Gradle":
+			family = "Tools"; break;
+			
+		case "IoT":
+			family = "IoT"; break;
+			
+		case "Java8":
+		case "FunctionalProgramming":
+			family = "Java8"; break;
+			
+		case "Kotlin" : 
+		case "Clojure" :		
+		case "Groovy" :
+		case "Ceylon": 
+		case "JVM langs" :
+		case "Scala":
+			family = "JVMLangs"; break;
+
+		case "Cache" : 
+		case "JavaEE" : 
+		case "Performance":
+		case "Compiler":
+		case "JavaFX":
+			family = "JVM"; break;
+
+		case "Containers" : 
+		case "Kubernetes":
+		case "Microservices":
+		case "RESTful":
+			family = "Microservices"; break;
+
+		case "noSQL" :
+		case "Bigdata":
+		case "Cassandra":
+		case "ElasticSearch":
+		case "MapReduce":
+		case "MongoDB":
+		case "Hadoop":
+			family = "noSQL"; break;
+		
+		case "Async" : 
+		case "Netty" : 
+		case "Event-driven":
+		case "DistributedSystems":
+		case "RxJava":
+			family = "AsyncDevelopment"; break;
+		
+		case "Android" : 
+			family = "Mobile"; break;
+		
+		case "AngularJS" : 
+		case "Play" : 
+		case "Akka" : 
+		case "Vert.x" : 
+		case "Frameworks":
+		case "jBPM":
+			family = "Frameworks"; break;
+			
+		default : family = "unknown"; 
+	}
+	return family;
+}
---
+++ _site/2016/README.md
@@ -0,0 +1,31 @@
+Merlin
+======
+Free template with [Twitter Bootstrap](http://getbootstrap.com/) framework.
+
+### Download
+
+[https://github.com/halibegic/merlin/archive/master.zip](https://github.com/halibegic/merlin/archive/master.zip)
+
+### Credits
+ * [jQuery](http://jquery.com/)
+ * [Bootstrap](http://getbootstrap.com/)
+ * [Font Awesome](http://fortawesome.github.io/Font-Awesome/)
+ * [Isotope](https://github.com/desandro/isotope)
+ * [ImagesLoaded](https://github.com/desandro/imagesloaded)
+ * [Animate.css](http://daneden.github.io/animate.css/)
+ * [Twitter Post Fetcher](http://jasonmayes.com/projects/twitterApi)
+ * [jQuery Scroll To](http://flesler.blogspot.com/2007/10/jqueryscrollto.html)
+ * [jQuery One Page Nav](https://github.com/davist11/jQuery-One-Page-Nav)
+ * [jQuery Appear](https://github.com/bas2k/jquery.appear/)
+ * [Unsplash](http://unsplash.com/)
+ * [Stockvault](https://www.stockvault.net/c/people)
+
+### Copyright and License
+
+Copyright 2014 [Hasan Alibegic](http://www.twitter.com/halibegic)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
