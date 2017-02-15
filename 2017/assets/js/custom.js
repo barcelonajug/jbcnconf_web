551355a39ab4ce79dc9d8ac51b7543adc5e90310
Miklos Csere <miklos.csere@netcentric.biz>
2017-02-15 10:08:55 +0100

    Ticket prices and Sponsors

--- 2017/assets/js/custom.js
+++ 2017/assets/js/custom.js
@@ -1,83 +1,91 @@
-jQuery(document).ready(function($){
-	
-	/* ------------------- Client Carousel --------------------- */
-
-	$('.clients-carousel').flexslider({
-	    animation: "slide",
-		easing: "swing",
-	    animationLoop: true,
-	    itemWidth: 188,
-	    itemMargin: 0,
-	    minItems: 1,
-	    maxItems: 5,
-		controlNav: false,
-		directionNav: false,
-		move: 1
-      });
+(function () {
 
 
-	/* ------------------ Back To Top ------------------- */
+    jQuery(document).ready(function ($) {
 
-	jQuery('#footer-menu-back-to-top a').click(function(){
-		jQuery('html, body').animate({scrollTop:0}, 300); 
-		return false; 
-	});
-	
-	/* ------------------ Progress Bar ------------------- */	
-	$(function() {
-		$(".meter > span").each(function() {
-			$(this)
-			.data("origWidth", $(this).width())
-			.width(0)
-			.animate({
-				width: $(this).data("origWidth")
-			}, 1200);
-		});
-	});
+        /* ------------------- Client Carousel --------------------- */
 
-	/* --------------------- Tabs ------------------------ */	
-
-		(function() {
-
-			var $tabsNav    = $('.tabs-nav'),
-				$tabsNavLis = $tabsNav.children('li'),
-				$tabContent = $('.tab-content');
-
-			$tabsNav.each(function() {
-				var $this = $(this);
-
-				$this.next().children('.tab-content').stop(true,true).hide()
-													 .first().show();
-
-				$this.children('li').first().addClass('active').stop(true,true).show();
-			});
-
-			$tabsNavLis.on('click', function(e) {
-				var $this = $(this);
-
-				$this.siblings().removeClass('active').end()
-					 .addClass('active');
-
-				$this.parent().next().children('.tab-content').stop(true,true).hide()
-															  .siblings( $this.find('a').attr('href') ).fadeIn();
-
-				e.preventDefault();
-			});
-
-		})();
-		
-			
-});
+        setTimeout(function () {
+            $('.clients-carousel').flexslider({
+                animation: "slide",
+                easing: "swing",
+                animationLoop: true,
+                itemWidth: 188,
+                itemMargin: 0,
+                minItems: 1,
+                maxItems: 5,
+                controlNav: false,
+                directionNav: false,
+                move: 1
+            });
+        }, 100);
 
 
-/* ------------------- Parallax --------------------- */
+        /* ------------------ Back To Top ------------------- */
 
-jQuery(document).ready(function($){
-	
-	$('#da-slider').cslider({
-		autoplay	: true,
-		bgincrement	: 450,
-		interval	: 5000
-	});
+        jQuery('#footer-menu-back-to-top a').click(function () {
+            jQuery('html, body').animate({scrollTop: 0}, 300);
+            return false;
+        });
 
-});
+        /* ------------------ Progress Bar ------------------- */
+        $(function () {
+            $(".meter > span").each(function () {
+                $(this)
+                    .data("origWidth", $(this).width())
+                    .width(0)
+                    .animate({
+                        width: $(this).data("origWidth")
+                    }, 1200);
+            });
+        });
+
+        /* --------------------- Tabs ------------------------ */
+
+        (function () {
+
+            var $tabsNav = $('.tabs-nav'),
+                $tabsNavLis = $tabsNav.children('li'),
+                $tabContent = $('.tab-content');
+
+            $tabsNav.each(function () {
+                var $this = $(this);
+
+                $this.next().children('.tab-content').stop(true, true).hide()
+                    .first().show();
+
+                $this.children('li').first().addClass('active').stop(true, true).show();
+            });
+
+            $tabsNavLis.on('click', function (e) {
+                var $this = $(this);
+
+                $this.siblings().removeClass('active').end()
+                    .addClass('active');
+
+                $this.parent().next().children('.tab-content').stop(true, true).hide()
+                    .siblings($this.find('a').attr('href')).fadeIn();
+
+                e.preventDefault();
+            });
+
+        })();
+
+        /* ------------------- Parallax --------------------- */
+        setTimeout(function () {
+            $('#da-slider').cslider({
+                autoplay: true,
+                bgincrement: 450,
+                interval: 5000
+            });
+        }, 100);
+
+
+        $('.testimonials-carousel').carousel({
+            namespace: "mr-rotato" // Defaults to “carousel”.
+        })
+
+    });
+
+
+}());
