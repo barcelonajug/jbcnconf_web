87b4f36d59de44b8d873dae86cbbe24da99433b0
Christian Sisti <ciscox83@Christians-MacBook-Pro.local>
2017-04-01 11:12:29 +0200

    Code review: move html content on scripts_bottom,html

--- 2017/assets/js/twitter-campaign.js
+++
@@ -1,2 +0,0 @@
-<script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
-<script type="text/javascript">twttr.conversion.trackPid('nx4fy', { tw_sale_amount: 0, tw_order_quantity: 0 });</script>
